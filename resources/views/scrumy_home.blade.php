d87ac657ca8589532f804c5f30e5c6e5daed92b4
Gleb Starkov <glebstarkov@gmail.com>
2016-08-22 14:28:08 +0800

    small refactoring

--- resources/views/scrumy_home.blade.php
+++ resources/views/scrumy_home.blade.php
@@ -8,7 +8,6 @@
 			<div class="panel panel-primary">
 					<div class="panel-heading scrumy-panel">Projects</div>
 					<div class="panel-body charts_container" id= "charts_container">
-
 						<script type="text/javascript">
 							$a =  document.getElementById("aUser").value;
 							var data2 = <?php echo json_encode($user); ?>;
@@ -30,78 +29,5 @@
 			</div>
 	</div>
 </footer>
-<script type="text/javascript">
-window.onload = function () {
-	for (var i = 0; i < data2.length; i++) {
-		data2[i]['todo'] = data2[i]['todo'].toString();
-		data2[i]['doing'] = data2[i]['doing'].toString();
-		data2[i]['done'] = data2[i]['done'].toString();
-		var project_id = data2[i]['project_id'];
-		var productivity_chart = "productivity_chart".concat(i);
-			if(data2[i]['todo']!= 0 && data2[i]['doing']!= 0 && data2[i]['done']!= 0){
-				var chart = new CanvasJS.Chart(productivity_chart, {
-
-					title:{
-						text: data2[i]['project_name']              
-					},
-					data: [              
-					{
-				// Change type to "doughnut", "line", "splineArea", etc.
-						type: "doughnut",
-						explodeOnClick : false,
-				 		indexLabelPlacement: "inside",
-				 		indexLabelFontSize: 14,
-				 		indexLabelFontColor: "#fafafa",
-						click: onClick,
-						dataPoints: [
-							{ label: "to-do",  y: data2[i]['todo'],indexLabel : data2[i]['todo']},
-							{ label: "doing", y: data2[i]['doing'],indexLabel : data2[i]['doing']},
-							{ label: "done", y: data2[i]['done'],indexLabel : data2[i]['done']}
-						]
-					}
-					]
-				});
-			}	
-			else {
-				var chart = new CanvasJS.Chart(productivity_chart, {
-					title:{
-						text: data2[i]['project_name']              
-					},
-					legend: {
-						cursor:"pointer",
-						horizontalAlign: "left", // "center" , "right"
-						verticalAlign: "center",  // "top" , "bottom"
-						fontSize: 15,
-						href: "{{URL::to('board')}}/" + project_id,
-						itemclick: function(e){
-							window.location = $(this).attr('href');
-						}
-					},
-					data: [              
-					{
-				// Change type to "doughnut", "line", "splineArea", etc.
-						type: "doughnut",
-						showInLegend: true,
-      					legendText: "clic to add card", 						
-						explodeOnClick : false,
-				 		indexLabelPlacement: "inside",
-				 		indexLabelFontSize: 14,
-				 		indexLabelFontColor: "#fafafa",
-						click: onClick,
-						dataPoints: [
-							{ label: "to-do",  y: data2[i]['todo'],indexLabel : data2[i]['todo']}
-						]
-					}
-					]
-				});
-			}				
-			chart.render();
-	}	
-}
-function onClick(e){
-	return false;
-	//window.location="{{URL::to('board')}}";
-}
-</script>
-
+<script  src="{{ asset('/custom_js/home.js') }}?v={{ config('app.script_version') }}"></script>
 @endsection
